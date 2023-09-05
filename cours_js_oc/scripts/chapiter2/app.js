4e1a16a5fb19a87b000894c9bdef3c886fe8a7d8
N30-Geek <dryikobu967@gmail.Com>
2023-09-05 17:23:36 +0200

    La methode de creation de elements avec createElement

--- cours_js_oc/scripts/chapiter2/app.js
+++ cours_js_oc/scripts/chapiter2/app.js
@@ -1,6 +1,21 @@
+// Les contenues des elements 
+let contenuTitre = "Azertype";
+let contenuParagraphe = "L'application pour apprendre a tapez plus vite au clavier";
+
+// Creations des elements de la pages
+let nouvelleElementDiv = document.createElement("div");
+let nouvelleElementTitre = document.createElement("h1");
+let nouvelleElementParagraphe = document.createElement("p");
+
+// ajout des contenue dans le paragraphe et le titre
+
+nouvelleElementParagraphe.textContent = contenuParagraphe;
+nouvelleElementTitre.textContent = contenuTitre;
+
+// ajout dans l'element principal body
+nouvelleElementDiv.appendChild(nouvelleElementTitre);
+nouvelleElementDiv.appendChild(nouvelleElementParagraphe); 
 
 
-
-const listMot = document.querySelector(".listMot");
-listMot.classList.add("coolman");
-listMot.classList.remove("photo");
+let body = document.querySelector("body");
+body.appendChild(nouvelleElementDiv);
