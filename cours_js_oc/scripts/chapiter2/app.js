e4fb4c1314f680bc4b79568fa72788689527d2e1
N30-Geek <dryikobu967@gmail.Com>
2023-09-05 17:28:58 +0200

    creation des elements html par interpolation

--- cours_js_oc/scripts/chapiter2/app.js
+++ cours_js_oc/scripts/chapiter2/app.js
@@ -3,19 +3,10 @@ let contenuTitre = "Azertype";
 let contenuParagraphe = "L'application pour apprendre a tapez plus vite au clavier";
 
 // Creations des elements de la pages
-let nouvelleElementDiv = document.createElement("div");
-let nouvelleElementTitre = document.createElement("h1");
-let nouvelleElementParagraphe = document.createElement("p");
-
-// ajout des contenue dans le paragraphe et le titre
-
-nouvelleElementParagraphe.textContent = contenuParagraphe;
-nouvelleElementTitre.textContent = contenuTitre;
-
-// ajout dans l'element principal body
-nouvelleElementDiv.appendChild(nouvelleElementTitre);
-nouvelleElementDiv.appendChild(nouvelleElementParagraphe); 
-
+let div = `
+    <h1>${contenuTitre}</h1>
+    <p>${contenuParagraphe}</p>
+`
 
 let body = document.querySelector("body");
-body.appendChild(nouvelleElementDiv);
+body.innerHTML = div;
